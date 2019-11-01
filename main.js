55989702902e1ffc7b51648dac7f7f7e1f72d080
tyush <tyushk@gmail.com>
2019-11-01 11:57:26 -0500

    add working assets and base code for cover

--- main.js
+++ main.js
@@ -3,25 +3,39 @@ var config = {
     width: 640,
     height: 480,
     scene: {
-        preload: init,
+        preload: preload,
         create: create,
-        update: ontick
+        update: update
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
     }
 };
 
 var game = new Phaser.Game(config);
 
-function init() {
+var cover;
+
+function preload() {
     this.load.image('background', 'assets/background.png');
     this.load.spritesheet('player', 'assets/player.png');
     this.load.spritesheet('enemy', 'assets/enemy.png');
     this.load.spritesheet('cpu', 'assets/enemygreyscale.png');
+    this.load.spritesheet('wall', 'assets/cover.png');
 }
 
 function create() {
-    this.add.image(640, 480, 'background');
+    this.add.image(320, 240, imgbackground);
+
+    cover = this.physics.add.staticGroup();
+    cover.create(400, 320, 'wall');
+    cover.create(100, 200, 'wall');
 }
 
-function ontick() {
+function update() {
 
 }
